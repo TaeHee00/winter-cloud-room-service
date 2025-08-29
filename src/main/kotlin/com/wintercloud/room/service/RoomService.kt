722e3eb34daf897a7fb2f28fc73c86c4a1da9258
Tae Hee Jang <kancth03@gmail.com>
2025-08-29 20:01:47 +0900

    feat: Room 생성 로직 일부 변경 및 본인 채팅룸 조회 기능 구현

--- src/main/kotlin/com/wintercloud/room/service/RoomService.kt
+++ src/main/kotlin/com/wintercloud/room/service/RoomService.kt
@@ -1,7 +1,9 @@
 package com.wintercloud.room.service
 
 import com.wintercloud.room.entity.Room
+import com.wintercloud.room.entity.RoomUser
 import com.wintercloud.room.repository.RoomRepository
+import com.wintercloud.room.repository.RoomUserRepository
 import com.wintercloud.room.utils.UUIDv7
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,19 +12,38 @@ import java.util.UUID
 @Service
 class RoomService(
     private val roomRepository: RoomRepository,
+    private val roomUserRepository: RoomUserRepository,
 ) {
+    @Transactional(readOnly = true)
+    suspend fun getRoomList(
+        userId: UUID,
+    ): List<Room> {
+        return roomUserRepository.findAllByUserId(userId)
+            // 사라진 방의 경우 제외하고 진행
+            .mapNotNull { roomRepository.findById(it.roomId) }
+            .toList()
+    }
+
     @Transactional
     suspend fun createRoom(
         roomName: String,
         createdBy: UUID,
     ) {
         // UUID 확인하기 -> User Service 요청
-        roomRepository.save(
+        val savedRoom = roomRepository.save(
             entity = Room(
                 id = UUIDv7.randomUUID(),
                 destination = UUIDv7.randomUUID(),
                 name = roomName,
                 createdBy = createdBy,
+            )
+        )
+
+        roomUserRepository.save(
+            entity = RoomUser(
+                id = UUIDv7.randomUUID(),
+                roomId = savedRoom.id,
+                userId = createdBy,
             )
         )
     }
